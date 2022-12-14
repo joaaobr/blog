4e6bf3ae8e642f28660f630beaf57d5b2612f69c
joaaobr <anacricris12@gmail.com>
2022-12-14 00:21:06 -0300

    added new route to update posts

--- backend/controllers/PostsController.js
+++ backend/controllers/PostsController.js
@@ -28,6 +28,25 @@ module.exports = {
         }
     },
 
+    async update(req, res) {
+        const { id } = req.params
+
+        if(req.body.email) return res.status(404).json({ message: "It is not valid to send email!" })
+
+        try {
+            const post = await Post.findById(id)
+
+            if(!post) return res.status(404).json({ message: "Id is not valid!" })
+
+            const PostToBeUpdate = await Post.findByIdAndUpdate(id, req.body, { now: true })
+
+            return res.status(200).json({ message: PostToBeUpdate })
+
+        } catch(err) {
+            return res.status(500).json(err)
+        }
+    },
+
     async posts(req, res) {
         try {
             const posts = await Post.find()
--- backend/routes.js
+++ backend/routes.js
@@ -9,6 +9,8 @@ module.exports = (app) => {
     app.post('/user/update/:id', UserController.update)
 
     app.post('/post/create', PostController.create)
+    
+    app.post('/post/update/:id', PostController.update)
 
     app.post('/post', PostController.posts)
 }
