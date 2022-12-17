dafcbd2f76a33c149ba6dbd21a22781fffc62357
joaaobr <anacricris12@gmail.com>
2022-12-17 16:38:18 -0300

    fix

---
+++ app/src/components/posts/GetPost.vue
@@ -0,0 +1,23 @@
+<template>
+<body>
+    <div>
+        <h1>{{title}}</h1>
+
+        <p>{{ post }}</p>
+    </div>
+</body>
+</template>
+
+<script>
+export default {
+    name: "GetPost",
+    data() {
+        return {
+            title: null,
+            post: null
+        }
+    }
+
+}
+</script>
+
--- app/src/views/CreatePost.vue
+++ app/src/views/CreatePost.vue
@@ -1,13 +1,17 @@
 <template>
+    <Navbar></Navbar>
     <CreatePost></CreatePost>
 </template>
 
 <script>
 import CreatePost from '@/components/posts/create/Create.vue';
+import Navbar from '@/components/navbar/Navbar.vue';
+
 export default { 
   name: 'HomeView',
   components: {
-    CreatePost
+    CreatePost,
+    Navbar
   }
 }
 </script>
