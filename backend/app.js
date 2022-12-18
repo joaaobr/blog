115dba1f6f1e2ca1b70a1e660ca7035da7697094
joaaobr <anacricris12@gmail.com>
2022-12-18 17:46:05 -0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -7,14 +7,14 @@ app.use(express.json());
 app.use(cors())
 
 app.use((req, res, next) =>  {
-    next()
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "POST")
     res.header(
         'Access-Control-Allow-Header',
         'Origin, X-Requested-With, Content-Type, Accept, authorization'
-    );
-    res.header("Access-Control-Allow-Credentials", true)
+        );
+        res.header("Access-Control-Allow-Credentials", true)
+        next()
 
 }
 )
