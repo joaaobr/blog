e1ffbc800e7a1d39a882c6b76a04817c60d225a6
joaaobr <anacricris12@gmail.com>
2022-12-19 14:41:26 -0300

    added name in navabar

--- app/src/components/navbar/Navbar.vue
+++ app/src/components/navbar/Navbar.vue
@@ -1,8 +1,36 @@
 <template src="./navbar.html"></template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    data() {
+        return {
+            name: null,
+            id: null
+        }
+    },
+
+    mounted() {
+        this.getUserData()
+    },
+
+    methods: {
+        async getUserData() {
+            const token = window.localStorage.getItem("token")
+
+            await axios.post("http://localhost:3000/user/getUserByToken", { token })
+            .then(data => {
+                console.log(data)
+                this.name = data.data.message.name
+                this.id = data.data.message._id
+            })
+            .catch(err => {
+                window.location.href = "/auth"
+            })
+        }
+    }
 }
 </script>
 
