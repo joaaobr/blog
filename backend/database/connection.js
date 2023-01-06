447af04482810d4fbee5819ca8b5414d0b4982c6
joaaobr <anacricris12@gmail.com>
2023-01-06 15:06:33 -0300

    added strictQuery

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-const { DB_HOST, DB_NAME, NODE_ENV, DB_URI } = process.env 
+const { DB_HOST, DB_NAME, NODE_ENV, DB_URI } = process.env
+
+mongoose.set("strictQuery", true);
 
 if (NODE_ENV === "development") {
     mongoose.connect(`mongodb://${DB_HOST}/${DB_NAME}`)
